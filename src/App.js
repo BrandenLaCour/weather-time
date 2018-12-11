c618d455830a0e6df533a1517dc40d77bcfe5f23
Branden <brandenlacour@gmail.com>
2018-12-10 23:05:27 -0600

    'updating more'
    icon changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import WeatherGrid from "./components/weatherGrid";
-import { currentWeek } from './components/utils/currentWeek';
-import Input from './components/utils/input'
+import { currentWeek } from "./components/utils/currentWeek";
+import Input from "./components/utils/input";
 import "./App.css";
 import axios from "axios";
 
@@ -9,53 +9,59 @@ import axios from "axios";
 
 class App extends Component {
   state = {
-    
-    
     // add routing to pick certain days, etc.
     // style with background images, etc.
     week: [
-      
-      { day: '', high: '', low: '', cast: "" },
-      { day: '', high: '', low: '', cast: "" },
-      { day: '', high: '', low: '', cast: "" },
-      { day: '', high: '', low: '', cast: "" },
-      { day: '', high: '', low: '', cast: "" },
-      { day: '', high: '', low: '', cast: "" },
-      { day: '', high: '', low: '', cast: "" }
+      { day: "", high: "", low: "", cast: "" },
+      { day: "", high: "", low: "", cast: "" },
+      { day: "", high: "", low: "", cast: "" },
+      { day: "", high: "", low: "", cast: "" },
+      { day: "", high: "", low: "", cast: "" },
+      { day: "", high: "", low: "", cast: "" },
+      { day: "", high: "", low: "", cast: "" }
     ],
-    toggle: {show: true},
-    location: {name: 'Chicago', current: '41.8781,-87.6298'}
+    toggle: { show: true },
+    location: { name: "Chicago", current: "41.8781,-87.6298" }
   };
   // refractor code to shorten this page, export to other components
-  componentWillMount(){
-   //creating the proper week layout depending on the day the app is rendered
+  componentWillMount() {
+    //creating the proper week layout depending on the day the app is rendered
     const newDate = new Date();
     const day = newDate.getDay();
-    let oldWeek = [...this.state.week]
-    const newWeek = currentWeek(day)
-    const week = oldWeek.map((e,i) => {
-      e.day = newWeek[i]
-      e.high = ''
-      e.low = ''
-      e.cast = 'partly-cloudy-day'
-      return e
-    })
-    
-    this.setState({week})
+    let oldWeek = [...this.state.week];
+    const newWeek = currentWeek(day);
+    const week = oldWeek.map((e, i) => {
+      e.day = newWeek[i];
+      e.high = "";
+      e.low = "";
+      e.cast = "partly-cloudy-day";
+      return e;
+    });
+
+    this.setState({ week });
   }
 
   // get the default weather (chicago) from the api
   async componentDidMount() {
     const { data } = await axios.get(
-      `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/bab642085489d446f31df424bdf3e506/${this.state.location.current}`
+      `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/bab642085489d446f31df424bdf3e506/${
+        this.state.location.current
+      }`
     );
-    console.log('cors worked')
+    console.log("cors worked");
     const { data: allDays } = data.daily;
-    
-   
+
     let week = [...this.state.week];
     //refractor to shorten code here
-    const days = [allDays[0], allDays[1], allDays[2],allDays[3],allDays[4], allDays[5], allDays[6]]
+    const days = [
+      allDays[0],
+      allDays[1],
+      allDays[2],
+      allDays[3],
+      allDays[4],
+      allDays[5],
+      allDays[6]
+    ];
 
     const fiveDay = week.map((e, i) => {
       e.high = Math.round(days[i].apparentTemperatureMax);
@@ -64,33 +70,41 @@ class App extends Component {
       e.day = week[i].day;
       return e;
     });
-    
-    week = fiveDay
-    
-    this.setState({ week});
+
+    week = fiveDay;
+
+    this.setState({ week });
   }
 
-  handleToggle=()=>{
-   let toggle = {...this.state.toggle}
-   toggle.show = !toggle.show
-   
-   this.setState({toggle})
-   
-  }
+  handleToggle = () => {
+    let toggle = { ...this.state.toggle };
+    toggle.show = !toggle.show;
+
+    this.setState({ toggle });
+  };
   // switch the location to the designated area
-   updateTest = async (e)=>{
-    const name = e.target.name
+  updateTest = async e => {
+    const name = e.target.name;
     const { data } = await axios.get(
-      `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/bab642085489d446f31df424bdf3e506/${e.target.value}`
+      `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/bab642085489d446f31df424bdf3e506/${
+        e.target.value
+      }`
     );
-    
+
     const { data: allDays } = data.daily;
-    
-   
+
     let week = [...this.state.week];
     //refractor to shorten code here
-    const days = [allDays[0], allDays[1], allDays[2],allDays[3],allDays[4], allDays[5], allDays[6]]
-    
+    const days = [
+      allDays[0],
+      allDays[1],
+      allDays[2],
+      allDays[3],
+      allDays[4],
+      allDays[5],
+      allDays[6]
+    ];
+
     const fiveDay = week.map((e, i) => {
       e.high = Math.round(days[i].apparentTemperatureMax);
       e.low = Math.round(days[i].apparentTemperatureMin);
@@ -98,36 +112,33 @@ class App extends Component {
       e.day = week[i].day;
       return e;
     });
-    
-    week = fiveDay
-    let location = {...this.state.location} 
-    location.name = name
-    this.handleToggle()
-    this.setState({ week, location});
-  }
 
-
-
+    week = fiveDay;
+    let location = { ...this.state.location };
+    location.name = name;
+    this.handleToggle();
+    this.setState({ week, location });
+  };
 
   render() {
     const { week, toggle, location } = this.state;
 
-  
-    
     return (
       <div id="background">
         <div className="row justify-content-center">
-          <img src='https://bit.ly/2PPfHJ5' alt=''></img>
-      
-        </div><Input toggle = {toggle.show} click ={this.handleToggle} loc={this.updateTest} label={location.name}></Input>
+          <img src="https://bit.ly/2PPfHJ5" alt="" />
+        </div>
+        <Input
+          toggle={toggle.show}
+          click={this.handleToggle}
+          loc={this.updateTest}
+          label={location.name}
+        />
         <WeatherGrid week={week} />
-        <p>By Branden LaCour, Powered By Dark Sky. 
-        </p>
+        <p>By Branden LaCour, Powered By Dark Sky.</p>
         <p>Currently under development</p>
         <a href="https://github.com/BrandenLaCour/weather-time">Github</a>
-
       </div>
-      
     );
   }
 }
